a199ef711834f9b312ba4890f67f4496f82a3ebb
Facundo Pereyra <36206720+fpereyrap@users.noreply.github.com>
2023-09-18 21:34:03 -0300

    Create Dile.mk

---
+++ Dile.mk
@@ -0,0 +1,13 @@
+## Casio CTS Keyboard Comparison
+
+| Feature/Model      | CTS-100       | CTS-200       | CTS-300       |
+|-------------------|--------------|--------------|--------------|
+| **Keys**              | 61 full-size | 61 full-size | 61 full-size |
+| **Tones**             | 120          | 400          | Approx. 500* |
+| **Rhythms**           | 70           | 77           | Approx. 90*  |
+| **Built-in Songs**    | 50           | 60           | Approx. 70*  |
+| **Max Polyphony**     | 32           | 48           | Approx. 64*  |
+| **Dance Music Mode**  | Yes          | Yes (with Voice) | Yes*     |
+| **Speakers**          | Two 2W       | Two 2W       | Two 2W*      |
+| **Connectivity**      | Audio out, Headphone | Audio out, Headphone, USB | Audio out, Headphone, USB* |
+| **Power**             | Battery/Optional adapter | Battery/Included adapter | Battery/Included adapter* |
